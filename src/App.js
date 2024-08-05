c582a98bd09dcb76d27df443ff5ac1b1561c0388
Sachendra79 <sachendragangwar120@gmail.com>
2024-08-05 11:48:14 +0530

    added the tailwind configuration

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div >
-     Welcome to React app fkdngkd
-    </div>
-  );
+    <h1 className="text-3xl font-bold underline text-black">
+      Hello world! how are you
+    </h1>
+  )
 }
 
 export default App;
