186b5ef8103d0a4a8608622d974aacbff0315380
Sachendra79 <sachendragangwar120@gmail.com>
2024-08-25 18:24:33 +0530

    Added the course option

--- src/components/core/pages/Homes.jsx
+++ src/components/core/pages/Homes.jsx
@@ -5,15 +5,15 @@ import { Link } from "react-router-dom"
 // Image and Video Import
 import Banner from "../assets/Images/banner.mp4"
 // Component Imports
-import Footer from "../components/common/Footer"
+import Footer from "../../common/Footer"
 import ReviewSlider from "../components/common/ReviewSlider"
-import CTAButton from "../components/core/HomePage/Button"
-import CodeBlocks from "../components/core/HomePage/CodeBlocks"
-import ExploreMore from "../components/core/HomePage/ExploreMore"
-import HighlightText from "../components/core/HomePage/HighlightText"
-import InstructorSection from "../components/core/HomePage/InstructorSection"
-import LearningLanguageSection from "../components/core/HomePage/LearningLanguageSection"
-import TimelineSection from "../components/core/HomePage/TimelineSection"
+import CTAButton from "../HomePage/Button"
+import CodeBlocks from "../HomePage/CodeBlocks"
+import ExploreMore from "../HomePage/ExploreMore"
+import HighlightText from "../HomePage/HighlightText"
+import InstructorSection from "../HomePage/InstructorSection"
+import LearningLanguageSection from "../HomePage/LearningLanguageSection"
+import TimelineSection from "../HomePage/TimelineSection"
 
 function Home() {
   return (
